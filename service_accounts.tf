00bd3800d4b6f141de9dda4f2657224b1786225b
tungUAS <Tung.Trinh@senacor.com>
2023-12-04 13:33:15 +0100

    add github sa

--- service_accounts.tf
+++ service_accounts.tf
@@ -2,3 +2,21 @@ resource "google_service_account" "github_service_account" {
   account_id   = "github-actions-woopick"
   display_name = "github-actions-woopick"
 }
+
+variable "iam_roles" {
+  type = map(string)
+  default = {
+    gcr_writer      = "roles/artifactregistry.writer",
+    storage_admin   = "roles/storage.admin",
+    wif_user        = "roles/iam.workloadIdentityUser",
+    cloud_sql_admin = "roles/cloudsql.admin",
+  }
+}
+
+resource "google_project_iam_member" "iam_members" {
+  for_each = var.iam_roles
+
+  project = var.project_id
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "tfc_gcp_dynamic_credentials" {
+  description = "Object containing GCP dynamic credentials configuration"
+  type = object({
+    default = object({
+      credentials = string
+    })
+    aliases = map(object({
+      credentials = string
+    }))
+  })
+}
+
+variable "project_id" {
+  type = string
+}
