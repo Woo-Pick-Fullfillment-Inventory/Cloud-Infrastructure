2febfa5ef117bbf28be20d7846ffb29077d9cc13
tungUAS <Tung.Trinh@senacor.com>
2023-12-04 13:37:08 +0100

    add github sa

--- variable.tf
+++ variable.tf
@@ -1,15 +1,3 @@
-variable "tfc_gcp_dynamic_credentials" {
-  description = "Object containing GCP dynamic credentials configuration"
-  type = object({
-    default = object({
-      credentials = string
-    })
-    aliases = map(object({
-      credentials = string
-    }))
-  })
-}
-
 variable "project_id" {
   type = string
 }
