4edd2d8d3e5d5cbce16a6e068a1b8130320722eb
tungUAS <Tung.Trinh@senacor.com>
2023-12-06 10:51:33 +0100

    add vpc and subnet

---
+++ gcr.tf
@@ -0,0 +1,42 @@
+// creating artifact registry repo over UI
+// we dont use terraform beta 
+// no clean up policies allowed in this version
+
+// todo: add provider beta later
+// https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_versions
+// todo: adjust the cleanup policies
+
+/* resource "google_artifact_registry_repository" "my-repo" {
+  provider      = google-beta
+  location      = var.region
+  repository_id = "my-repository-${local.name_suffix}"
+  description   = "example docker repository with cleanup policies-${local.name_suffix}"
+  format        = "DOCKER"
+  cleanup_policy_dry_run = false
+  cleanup_policies {
+    id     = "delete-prerelease"
+    action = "DELETE"
+    condition {
+      tag_state    = "TAGGED"
+      tag_prefixes = ["alpha", "v0"]
+      older_than   = "2592000s"
+    }
+  }
+  cleanup_policies {
+    id     = "keep-tagged-release"
+    action = "KEEP"
+    condition {
+      tag_state             = "TAGGED"
+      tag_prefixes          = ["release"]
+      package_name_prefixes = ["webapp", "mobile"]
+    }
+  }
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+    most_recent_versions {
+      package_name_prefixes = ["webapp", "mobile", "sandbox"]
+      keep_count            = 5
+    }
+  }
+} */
---
+++ vpc.tf
@@ -0,0 +1,14 @@
+resource "google_compute_network" "sql_vpc_network" {
+  project                 = var.project_id
+  name                    = "vpc-network"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
+
+resource "google_compute_subnetwork" "todo_subnet" {
+  name          = "sql-subnet"
+  region        = var.region
+  network       = google_compute_network.sql_vpc_network
+  ip_cidr_range = "10.10.10.0/28"
+  project       = var.project_id
+}
