dc50663cb0e0ffb9c8bb22447cdfdb118c379735
tungUAS <Tung.Trinh@senacor.com>
2023-12-08 21:16:37 +0100

    add database

--- spanner.tf
+++ spanner.tf
@@ -10,30 +10,30 @@ resource "google_spanner_database" "database" {
   name     = "my-database"
   ddl = [
    <<-EOF
-   CREATE TABLE app_users (
-    app_user_id STRING(255) NOT NULL,
-    app_email STRING(255) NOT NULL,
-    app_username STRING(255) NOT NULL,
-    app_password STRING(255) NOT NULL,
-    app_url STRING(255) NOT NULL,
-    authenticated BOOL NOT NULL
-  ) PRIMARY KEY (app_user_id)
+    CREATE TABLE app_users (
+      app_user_id STRING(255) NOT NULL,
+      app_email STRING(255) NOT NULL,
+      app_username STRING(255) NOT NULL,
+      app_password STRING(255) NOT NULL,
+      app_url STRING(255) NOT NULL,
+      authenticated BOOL NOT NULL
+    ) PRIMARY KEY (app_user_id)
   EOF
   ,<<-EOF
     CREATE UNIQUE INDEX EmailUniqueIndex ON app_users (app_email);
   EOF
   ,<<-EOF
-  CREATE TABLE app_users_to_woo_users (
-      app_user_id STRING(255) NOT NULL,
-      woo_user_id STRING(255) NOT NULL
-  ) PRIMARY KEY (app_user_id, woo_user_id);
+    CREATE TABLE app_users_to_woo_users (
+        app_user_id STRING(255) NOT NULL,
+        woo_user_id STRING(255) NOT NULL
+    ) PRIMARY KEY (app_user_id, woo_user_id)
   EOF
   ,<<-EOF
-  CREATE TABLE woo_users (
-      woo_user_id STRING(255) NOT NULL,
-      woo_token STRING(255) NOT NULL,
-      woo_secret STRING(255) NOT NULL
-  ) PRIMARY KEY (woo_user_id);
+    CREATE TABLE woo_users (
+        woo_user_id STRING(255) NOT NULL,
+        woo_token STRING(255) NOT NULL,
+        woo_secret STRING(255) NOT NULL
+    ) PRIMARY KEY (woo_user_id)
   EOF
   ]
   deletion_protection = false
