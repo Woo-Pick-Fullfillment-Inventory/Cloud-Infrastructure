6afa8f50399935ecb645cfce627af567b9a105fe
tungUAS <Tung.Trinh@senacor.com>
2024-02-02 22:40:24 +0100

    add daily backup

--- firestore.tf
+++ firestore.tf
@@ -1,4 +1,4 @@
-resource "google_firestore_database" "firestore-database" {
+resource "google_firestore_database" "woo_pick_firestore_database" {
   project                           = var.project_id
   name                              = "woo-pick-inventory-firestore"
   location_id                       = "eur3"
@@ -7,5 +7,14 @@ resource "google_firestore_database" "firestore-database" {
   app_engine_integration_mode       = "DISABLED"
   point_in_time_recovery_enablement = "POINT_IN_TIME_RECOVERY_ENABLED"
   delete_protection_state           = "DELETE_PROTECTION_ENABLED"
-  deletion_policy                   = "ABANDON"
+  deletion_policy                   = "DELETE"
+}
+
+resource "google_firestore_backup_schedule" "woo_pick_firestore_daily_backup" {
+  project  = "my-project-name"
+  database = google_firestore_database.woo_pick_firestore_database.name
+
+  retention = "604800s" // 7 days (maximum possible value for daily backups)
+
+  daily_recurrence {}
 }
